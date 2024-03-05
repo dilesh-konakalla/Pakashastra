dfd6fbc31fce0e3df9a49824a63cab3407b24805
dileshkonakalla <2100031211@kluniversity.in>
2024-03-05 18:48:37 +0530

    commit

--- src/components/user.js
+++ src/components/user.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import Header from './header';
 import Navbar from './Navbar';
-import { Container,Box } from '@mui/material';
+import { Container, Box } from '@mui/material';
 import '../static/user.css'; // Import the CSS file
-
 
 const User = () => {
   const [recipe, setRecipe] = useState({
     name: '',
-    ingredients: [''], // Use an array to capture multiple ingredients
-    procedure: [''],  // Use an array to capture multiple procedure steps
-    category: '', 
+    ingredients: [''],
+    procedure: [''],
+    category: '',
   });
 
   const backgroundStyle = {
@@ -22,42 +21,26 @@ const User = () => {
 
   const handleInputChange = (e, index, field) => {
     const { value } = e.target;
-  
-    // Copy the current state
     const updatedRecipe = { ...recipe };
-  
-    // If the index is null, update the field directly
+
     if (index === null) {
       updatedRecipe[field] = value;
     } else {
-      // If the index is not null, update the specific field based on the index
       updatedRecipe[field][index] = value;
     }
-  
-    // Update the state
+
     setRecipe(updatedRecipe);
   };
-  
 
   const handleAddField = (field) => {
-    // Copy the current state
     const updatedRecipe = { ...recipe };
-
-    // Add a new empty string to the specified field array
     updatedRecipe[field].push('');
-
-    // Update the state
     setRecipe(updatedRecipe);
   };
 
   const handleRemoveField = (index, field) => {
-    // Copy the current state
     const updatedRecipe = { ...recipe };
-
-    // Remove the element at the specified index from the field array
     updatedRecipe[field].splice(index, 1);
-
-    // Update the state
     setRecipe(updatedRecipe);
   };
 
@@ -74,14 +57,13 @@ const User = () => {
       });
 
       const data = await response.json();
-
       console.log(data);
 
       setRecipe({
         name: '',
-        ingredients: [''], // Reset ingredients after submission
-        procedure: [''],   // Reset procedure after submission
-        category: '', 
+        ingredients: [''],
+        procedure: [''],
+        category: '',
       });
     } catch (error) {
       console.error(error);
@@ -100,84 +82,125 @@ const User = () => {
     'Snacks Recipes',
     'Winter Recipes',
   ];
-  const constyle = {
-    color: '#ea4f02',
-    padding: '50px',
-    paddingLeft: '40px'
-  }; 
-   const boxStyle = {
-    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+
+  const bodyStyle = {
+    overflow: 'hidden', // Hide the main scrollbar
+  };
+
+  const formContainerStyle = {
+    color: '#2f4e1b',
     padding: '20px',
-    borderRadius: '8px',
-    height : '50vh',
-    width : '70vh',
-  };   
+    width: '50%',
+    margin: 'auto',
+    overflowY: 'auto', // Make the form scrollable
+    height: '80vh', // Set the maximum height for the form
+  };
+
+  const formStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+  };
+
+  const inputStyle = {
+    margin: '5px',
+    padding: '8px',
+    borderRadius: '5px',
+    border: '1px solid #ccc',
+    width: '100%',
+  };
+
+  const buttonStyle = {
+    margin: '10px',
+    padding: '10px',
+    backgroundColor: '#5d9b36',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  };
 
   return (
     <div style={backgroundStyle}>
       <Header />
       <Navbar />
-        <Container style={constyle}>
-        <Box style={boxStyle}><form onSubmit={handleSubmit}>
-        <h1>Cook a new recipe...?</h1>
-                <label>
-                  Name:
+      <p></p>
+      <Container style={formContainerStyle}>
+        <Box style={formStyle}>
+          <form onSubmit={handleSubmit}>
+            <h1>Cook a new recipe...?</h1>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={recipe.name}
+                onChange={(e) => handleInputChange(e, null, 'name')}
+                style={inputStyle}
+              />
+            </label>
+            <label>
+              Ingredients:
+              {recipe.ingredients.map((ingredient, index) => (
+                <div key={index}>
                   <input
-                  type="text"
-                  name="name"
-                  value={recipe.name}
-                  onChange={(e) => handleInputChange(e, null, 'name')}
-                />
-
-                </label>
-                <br />
-                <label>
-                  Ingredients:
-                  {recipe.ingredients.map((ingredient, index) => (
-                    <div key={index}>
-                      <input
-                        type="text"
-                        value={ingredient}
-                        onChange={(e) => handleInputChange(e, index, 'ingredients')}
-                      />
-                      <button type="button" onClick={() => handleRemoveField(index, 'ingredients')}>Remove</button>
-                    </div>
-                  ))}
-                  <button type="button" onClick={() => handleAddField('ingredients')}>Add Ingredient</button>
-                </label>
-                <br />
-                <label>
-                  Procedure:
-                  {recipe.procedure.map((step, index) => (
-                    <div key={index}>
-                      <textarea
-                        value={step}
-                        onChange={(e) => handleInputChange(e, index, 'procedure')}
-                      />
-                      <button type="button" onClick={() => handleRemoveField(index, 'procedure')}>Remove</button>
-                    </div>
-                  ))}
-                  <button type="button" onClick={() => handleAddField('procedure')}>Add Step</button>
-                </label>
-                <br />
-                <label>
-                  Category:
-                  <select
-                    name="category"
-                    value={recipe.category}
-                    onChange={(e) => handleInputChange(e, null, 'category')}
-                  >
-                    {categoryOptions.sort().map((option, index) => (
-                      <option key={index} value={option}>
-                        {option}
-                      </option>
-                    ))}
-                  </select>
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-              </form></Box>
-        </Container>
+                    type="text"
+                    value={ingredient}
+                    onChange={(e) => handleInputChange(e, index, 'ingredients')}
+                    style={inputStyle}
+                  />
+                  <button type="button" onClick={() => handleRemoveField(index, 'ingredients')} style={buttonStyle}>
+                    Remove
+                  </button>
+                </div>
+              ))}
+              <button type="button" onClick={() => handleAddField('ingredients')} style={buttonStyle}>
+                Add Ingredient
+              </button>
+            </label>
+            <p></p>
+            <label>
+              Procedure:
+              {recipe.procedure.map((step, index) => (
+                <div key={index}>
+                  <textarea
+                    value={step}
+                    onChange={(e) => handleInputChange(e, index, 'procedure')}
+                    style={inputStyle}
+                  />
+                  <button type="button" onClick={() => handleRemoveField(index, 'procedure')} style={buttonStyle}>
+                    Remove
+                  </button>
+                </div>
+              ))}
+              <button type="button" onClick={() => handleAddField('procedure')} style={buttonStyle}>
+                Add Step
+              </button>
+            </label>
+            <p></p>
+            <label>
+              Category:
+              <select
+                name="category"
+                value={recipe.category}
+                onChange={(e) => handleInputChange(e, null, 'category')}
+                style={inputStyle}
+              >
+                {categoryOptions.sort().map((option, index) => (
+                  <option key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <button type="submit" style={buttonStyle}>
+              Submit
+            </button>
+          </form>
+        </Box>
+      </Container>
+      <style>{`body {${Object.entries(bodyStyle).map(([key, value]) => `${key}: ${value};`).join('')}}`}</style>
     </div>
   );
 };
